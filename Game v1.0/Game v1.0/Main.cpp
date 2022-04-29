733d7042e427a3eab11fec273a1a195eaa7ef95a
theqoeo <theqoeo@naver.com>
2022-04-29 20:53:10 +0900

    game

--- Game v1.0/Game v1.0/Main.cpp
+++ Game v1.0/Game v1.0/Main.cpp
@@ -33,11 +33,17 @@ const int MAX = 2;
 const int Scene_Logo = 0;
 const int Scene_Menu = 1;
 const int Scene_Stage = 2;
-const int Scene_Exit = 3;
+const int Scene_Store = 3;
+const int Scene_PlayerData = 4;
+const int Scene_Exit = 5;
 
 int SceneState = 0;
 
 int Check = 1;
+
+const int ISelect = 0;
+const int Width = 40;
+const int Height = 5;
 
 DWORD SetnameTime = 0;
 
@@ -48,6 +54,7 @@ typedef struct tagInfo
 	int IExp;
 	int ILevel;
 	int IGold;
+	int IClass;
 
 	float FAttack;
 	float FDefense;
@@ -62,7 +69,7 @@ typedef struct tagObject
 char* SetName();
 void InitializePlayer(OBJECT* _Warrior, OBJECT* _Archer, OBJECT* _Magician);
 void SceneManager(OBJECT* _Player, OBJECT* _Enemy);
-void MenuScene();
+void MenuScene(OBJECT* _Player);
 void PlayerScene(OBJECT* _Player);
 void StageScene(OBJECT* _Player, OBJECT* _Enemy);
 void EnemyScene(OBJECT* _Enemy);
@@ -73,6 +80,8 @@ void SetPosition(int _x, int _y, char* _str, int _Color);
 void FirstScene();
 void GameStrat();
 void GameStory();
+void PlayerData(OBJECT* _Player);
+void Store(OBJECT* _Player);
 
 int main(void)
 {
@@ -110,7 +119,22 @@ int main(void)
 			system("cls");
 
 			printf_s("Player : %s\n", Player->Name);
-
+			if (Player->Info.IClass == 1)
+			{
+				printf_s("Class : 전사\n");
+			}
+			else if (Player->Info.IClass == 2)
+			{
+				printf_s("Class : 궁수\n");
+			}
+			else if (Player->Info.IClass == 3)
+			{
+				printf_s("Class : 마법사\n");
+			}
+			else
+			{
+				printf_s("Class : 없음\n");
+			}
 			// ** 게임 루프
 			SceneManager(Player, Monster);
 		}
@@ -152,10 +176,16 @@ void SceneManager(OBJECT * _Player, OBJECT * _Enemy)
 		GameStory();
 		break;
 	case Scene_Menu:
-		MenuScene();
+		MenuScene(_Player);
 		break;
 	case Scene_Stage:
 		StageScene(_Player, _Enemy);
+		break;
+	case Scene_Store:
+		Store(_Player);
+		break;
+	case Scene_PlayerData:
+		PlayerData(_Player);
 		break;
 	case Scene_Exit:
 		exit(NULL);// ** 프로그램 종료
@@ -163,11 +193,11 @@ void SceneManager(OBJECT * _Player, OBJECT * _Enemy)
 	}
 }
 
-void MenuScene()
+void MenuScene(OBJECT* _Player)
 {
 	printf_s("MenuScene\n");
 
-	printf_s("1. 이동\n2. 상점\n3.  종료\n입력 : ");
+	printf_s("1. 이동\n2. 상점\n3. 상태\n4. 종료\n입력 : ");
 
 	int i = 0;
 	scanf_s("%d", &i);
@@ -175,13 +205,39 @@ void MenuScene()
 	if (i == 1)
 		SceneState = Scene_Stage;
 	else if (i == 2)
+		SceneState = Scene_Store;
+	else if (i == 3)
+		SceneState = Scene_PlayerData;
+	else if (i == 4)
 		SceneState = Scene_Exit;
+	else
+	{
+		printf_s("잘못 누르셨습니다. \n");
+		Sleep(1500);
+		system("cls");
+		printf_s("Player : %s\n", _Player->Name);
+		if (_Player->Info.IClass == 1)
+		{
+			printf_s("Class : 전사\n");
+		}
+		else if (_Player->Info.IClass == 2)
+		{
+			printf_s("Class : 궁수\n");
+		}
+		else if (_Player->Info.IClass == 3)
+		{
+			printf_s("Class : 마법사\n");
+		}
+		else
+		{
+			printf_s("Class : 없음\n");
+		}
+		MenuScene(_Player);
+	}
 }
 
 void InitializePlayer(OBJECT * _Warrior, OBJECT * _Archer, OBJECT * _Magician) // 플레이어 직업 선택
 {
-	int Width = 40;
-	int Height = 5;
 	int i = 0;
 
 	SetPosition(Width, Height, (char*)"1.전사 2.궁수 3. 마법사\n", 15);
@@ -194,13 +250,14 @@ void InitializePlayer(OBJECT * _Warrior, OBJECT * _Archer, OBJECT * _Magician) /
 		{
 			_Warrior->Name = SetName();
 
+			_Warrior->Info.IClass = 1;
 			_Warrior->Info.FAttack = 15;
 			_Warrior->Info.FDefense = 10;
 			_Warrior->Info.IExp = 0;
 			_Warrior->Info.IHP = 100;
 			_Warrior->Info.IMP = 50;
 			_Warrior->Info.ILevel = 1;
-			_Warrior->Info.IGold = 0;
+			_Warrior->Info.IGold = 20;
 			SetPosition(Width, Height, (char*)"당신의 직업은 전사입니다. \n", 15);
 			break;
 		}
@@ -208,13 +265,14 @@ void InitializePlayer(OBJECT * _Warrior, OBJECT * _Archer, OBJECT * _Magician) /
 		{
 			_Archer->Name = SetName();
 
+			_Archer->Info.IClass = 2;
 			_Archer->Info.FAttack = 20;
 			_Archer->Info.FDefense = 5;
 			_Archer->Info.IExp = 0;
 			_Archer->Info.IHP = 80;
 			_Archer->Info.IMP = 50;
 			_Archer->Info.ILevel = 1;
-			_Archer->Info.IGold = 0;
+			_Archer->Info.IGold = 20;
 			SetPosition(Width, Height, (char*)"당신의 직업은 궁수입니다. \n", 15);
 			break;
 		}
@@ -222,13 +280,14 @@ void InitializePlayer(OBJECT * _Warrior, OBJECT * _Archer, OBJECT * _Magician) /
 		{
 			_Magician->Name = SetName();
 
+			_Magician->Info.IClass = 3;
 			_Magician->Info.FAttack = 10;
 			_Magician->Info.FDefense = 5;
 			_Magician->Info.IExp = 0;
 			_Magician->Info.IHP = 70;
 			_Magician->Info.IMP = 100;
 			_Magician->Info.ILevel = 1;
-			_Magician->Info.IGold = 0;
+			_Magician->Info.IGold = 20;
 			SetPosition(Width, Height, (char*)"당신의 직업은 마법사입니다. \n", 15);
 			break;
 		}
@@ -269,46 +328,146 @@ void PlayerScene(OBJECT* _Player)
 	}
 }
 
-void StageScene(OBJECT * _Player, OBJECT * _Enemy)
+void Store(OBJECT* _Player) // 상점
 {
-	// ** 전투
-	int Width = 40;
-	int Height = 5;
-	int ISelect = 0;
+	if (_Player->Info.IClass == 1)
+	{
+		int Choiec = 0;
+		system("cls");
+		printf_s("Player : %s\n", _Player->Name);
+		printf_s("Class : 전사\n");
+		printf_s("1. 무기\n2. 방어구\n3. 장신구\n4. 메인메뉴\n");
+		scanf_s("%d", &Choiec);
+		system("cls");
 
-	OBJECT* Objects[MAX];
+		if (Choiec == 1)
+		{
+			int Weapon = 0;
+			printf_s("Player : %s\n", _Player->Name);
+			printf_s("Class : 전사\n");
+			printf_s("1. 녹슨 검 10 Gold\n2. 강철 검 20 Gold\n3. 짱짱샌검 30 Gold\n4. 메인메뉴\n");
+			scanf_s("%d", &Weapon);
+			system("cls");
 
-	OBJECT* Player = (OBJECT*)malloc(sizeof(OBJECT));
-	InitializePlayer(Player, Player, Player);
+			if (Weapon == 1 && _Player->Info.IGold >= 10)
+			{
+				int WChoice = 0;
+				printf_s("Player : %s\n", _Player->Name);
+				printf_s("Class : 전사\n");
+				printf_s("녹슨 검 효과 : Att + 10\n");
+				printf_s("1. 구입\n2. 취소\n");
+				printf_s("구입하시겠습니까? : ");
+				scanf_s("%d", &WChoice);
+				system("cls");
 
-	OBJECT* Monster = (OBJECT*)malloc(sizeof(OBJECT));
-	InitializeEnemy(Monster);
+				if (WChoice == 1)
+				{
+					printf_s("Player : %s\n", _Player->Name);
+					printf_s("Class : 전사\n");
+					printf_s("녹슨 검을 구입했습니다.\n");
+					printf_s("MenuScene로 돌아갑니다.\n");
+					Sleep(1000);
+					system("cls");
+					_Player->Info.FAttack = _Player->Info.FAttack + 10;
+					_Player->Info.IGold = _Player->Info.IGold - 10;
+					MenuScene(_Player);
+				}
+				else if (WChoice == 2)
+				{
+					printf_s("Player : %s\n", _Player->Name);
+					printf_s("Class : 전사\n");
+					printf_s("구입을 취소했습니다. \n");
+					printf_s("상점창으로 되돌아갑니다.");
+					Sleep(1000);
+					Store(_Player);
+				}
+				else
+				{
+					printf_s("Player : %s\n", _Player->Name);
+					printf_s("Class : 전사\n");
+					printf_s("잘못 선택하셨습니다.\n");
+					printf_s("상점창으로 돌아갑니다.\n");
+					Sleep(1000);
+					Store(_Player);
+				}
+			}
+			else if ((Weapon == 1 && _Player->Info.IGold < 10))
+			{
+				printf_s("골드가 부족합니다. \n");
+				printf_s("MenuScene로 돌아갑니다.\n");
+				Sleep(1000);
+				system("cls");
+				printf_s("Player : %s\n", _Player->Name);
+				printf_s("Class : 전사\n");
+				MenuScene(_Player);
+			}
+		}
+	}
+}
+
+void PlayerData(OBJECT* _Player) // 플레이어 상태창
+{
+	if (_Player->Info.IClass == 1)
+	{
+		printf_s("당신의 직업은 전사입니다.\n");
+		printf_s("HP : %d\n", _Player->Info.IHP);
+		printf_s("MP : %d\n", _Player->Info.IMP);
+		printf_s("Att : %f\n", _Player->Info.FAttack);
+		printf_s("Dtt : %f\n", _Player->Info.FDefense);
+		printf_s("Exp : %d\n", _Player->Info.IExp);
+		printf_s("Level : %d\n", _Player->Info.ILevel);
+		printf_s("Gold : %d\n", _Player->Info.IGold);
+	}
+	else if (_Player->Info.IClass == 2)
+	{
+		printf_s("당신의 직업은 궁수입니다.\n");
+		printf_s("HP : %d\n", _Player->Info.IHP);
+		printf_s("MP : %d\n", _Player->Info.IMP);
+		printf_s("Att : %f\n", _Player->Info.FAttack);
+		printf_s("Dtt : %f\n", _Player->Info.FDefense);
+		printf_s("Exp : %d\n", _Player->Info.IExp);
+		printf_s("Level : %d\n", _Player->Info.ILevel);
+		printf_s("Gold : %d\n", _Player->Info.IGold);
+	}
+	else if (_Player->Info.IClass == 3)
+	{
+		printf_s("당신의 직업은 마법사입니다.\n");
+		printf_s("HP : %d\n", _Player->Info.IHP);
+		printf_s("MP : %d\n", _Player->Info.IMP);
+		printf_s("Att : %f\n", _Player->Info.FAttack);
+		printf_s("Dtt : %f\n", _Player->Info.FDefense);
+		printf_s("Exp : %d\n", _Player->Info.IExp);
+		printf_s("Level : %d\n", _Player->Info.ILevel);
+		printf_s("Gold : %d\n", _Player->Info.IGold);
+	}
+
+	int i = 0;
+	printf_s("메뉴로 돌아가실거면 1을 눌러주세요. \n");
+	scanf_s("%d", &i);
+	
+	switch (i)
+	{
+	case 1:
+		system("cls");
+		SceneState = Scene_Menu;
+		break;
+	default:
+		printf_s("잘못 누르셨습니다.");
+		Sleep(2000);
+		system("cls");
+		PlayerData(_Player);
+		break;
+	}
+}
+
+void StageScene(OBJECT * _Player, OBJECT * _Enemy) // 스테이지 몬스터와 전투
+{
 	SetPosition(Width, Height, (char*)"StageScene\n", 15);
 	SetPosition(Width, Height, (char*)"1. 공격 2. 도망\n", 15);
 	SetPosition(Width, Height + 1, (char*)"몬스터를 마주쳤습니다.\n", 15);
 	SetPosition(Width, Height + 2, (char*)"선택 : ", 15);
 	scanf_s("%d", &ISelect);
 
-	switch (ISelect)
-	{
-	case 1:
-		if (Objects[PLAYER]->Info.FAttack > Objects[ENEMY]->Info.FDefense)
-		{
-			Objects[ENEMY]->Info.IHP -= Objects[PLAYER]->Info.FAttack - Objects[ENEMY]->Info.FDefense;
-		}
-		else
-			Objects[ENEMY]->Info.IHP -= 1;
-		if (Objects[ENEMY]->Info.FAttack > Objects[PLAYER]->Info.FDefense)
-		{
-			Objects[PLAYER]->Info.IHP -= Objects[ENEMY]->Info.FAttack - Objects[PLAYER]->Info.FDefense;
-		}
-		else
-			Objects[PLAYER]->Info.IHP -= 1;
-		break;
-	default:
-		break;
-	}
-	SceneState++;
 
 	PlayerScene(_Player);
 	EnemyScene(_Enemy);
@@ -347,7 +506,6 @@ void SetPosition(int _x, int _y, char* _str, int _Color)
 void FirstScene()// 게임시작 화면
 {
 	int Width = (50) - (strlen("RRRRRRRRRRRRRRRRR   PPPPPPPPPPPPPPPPP           GGGGGGGGGGGGG\n") / 2);
-	int Height = 5;
 	int n = 0;
 
 	for (int i = 0; i < 5; i++)
@@ -377,8 +535,6 @@ void FirstScene()// 게임시작 화면
 
 void GameStrat()// 게임 시작, 나가기 선택
 {
-	int Width = 40;
-	int Height = 5;
 	int ISelect = 0;
 
 	SetPosition(Width, Height, (char*)"1. 시작하기 2. 나가기\n", 15);
@@ -414,9 +570,6 @@ void GameStrat()// 게임 시작, 나가기 선택
 
 void GameStory()// 게임 스토리
 {
-	int Width = 40;
-	int Height = 5;
-
 	SetPosition(Width, Height, (char*)"마왕이 세상을 정복할려고 하고 있습니다.\n", 15);
 	SetPosition(Width, Height + 1, (char*)"몬스터를 잡고 강해져서 마왕을 퇴치해주세요,\n", 15);
 	Sleep(2000);
